820a98a495c97bbb609616ba606633051adc0fa7
J W <woodjon@gmail.com>
2020-04-05 13:57:03 -0400

    Get prediction service working

---
+++ MLNetService/Services/PredictionService.cs
@@ -0,0 +1,32 @@
+﻿using Grpc.Core;
+using Microsoft.ML;
+using System.Threading.Tasks;
+
+namespace MLNetService.Services
+{
+    public class PredictionService : Prediction.PredictionBase
+    {
+        private readonly MLContext _context;
+        private readonly PredictionEngine<HousingRequest, HousingReply> _predictionEngine;
+
+        public PredictionService()
+        {
+            _context = new MLContext();
+
+            var model = _context.Model.Load("./Model/housing-model.zip", out DataViewSchema inputSchema);
+
+            _predictionEngine = _context.Model.CreatePredictionEngine<HousingRequest, HousingReply>(model, inputSchema: inputSchema);
+        }
+
+        public override async Task<HousingReply> Predict(HousingRequest request, ServerCallContext context)
+        {
+            var response = new HousingReply();
+
+            var prediction = _predictionEngine.Predict(request);
+
+            response.Score = prediction.Score;
+
+            return await Task.FromResult(response);
+        }
+    }
+}
---
+++ MLNet_gRPC_Client/Program.cs
@@ -0,0 +1,36 @@
+﻿using Grpc.Net.Client;
+using MLNetService;
+using System;
+using System.Threading.Tasks;
+
+namespace MLNet_gRPC_Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+
+            var client = new Prediction.PredictionClient(channel);
+
+            var request = new HousingRequest
+            {
+                Longitude = -122.25f,
+                Latitude = 37.85f,
+                HousingMedianAge = 55.0f,
+                TotalRooms = 1627.0f,
+                TotalBedrooms = 235.0f,
+                Population = 322.0f,
+                Households = 120.0f,
+                MedianIncome = 8.3014f,
+                OceanProximity = "NEAR BAY"
+            };
+
+            var response = await client.PredictAsync(request);
+
+            Console.WriteLine($"Prediction is {response.Score.ToString("c")}");
+
+            Console.ReadLine();
+        }
+    }
+}
